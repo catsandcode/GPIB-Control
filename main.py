8e2ac2710efa18815c66802875597e325a4668e4
catsandcode <jc.roth73@gmail.com>
2017-06-24 00:11:44 -0700

    Cleaned up and added documentation

--- main.py
+++ main.py
@@ -1,123 +1,26 @@
-import time, math, wrapper
+import time, wrapper
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-def main():
-    wrapper.initialize()
-
-    # Constants
-    chopper_sweep_start = 1
-    chopper_sweep_stop = 120
-    chopper_sweep_step = 1
-    sample_rate = 512
-
-    # Setup sweeper
-    wrapper.set_continuous_wave_freq(200)
-    wrapper.set_power(9.5)
-
-    # Setup chopper
-    wrapper.set_chopper_amplitude(1)
-    wrapper.set_chopper_sweep_on(False)
-
-    # Setup lock-in
-    wrapper.set_time_constant(300.0)
-    wrapper.set_low_pass_slope(18)
-    wrapper.set_sensitivity(0.005)
-    wrapper.set_sample_rate(sample_rate)
-    wrapper.set_data('R', 'Theta')
-
-    # Start the scan
-    wrapper.start_scan()
-    wrapper.start_sweep()
-    start_time = time.time()
-
-    # Sleep
-    time.sleep(20)
-
-    # Stop the scan and record the time
-    wrapper.stop_scan()
-    stop_time = time.time()
-
-    # Get data from the lock-in amplifier
-    data_no_frequency = wrapper.get_data()
-    (data_rows, data_columns) = data_no_frequency.shape
-
-    # Figure out the frequency at each data point
-    daq_time = stop_time - start_time
-    freq_per_sec = (chopper_sweep_stop - chopper_sweep_start) / daq_time
-
-    frequency = []
-    for i in range(
-            data_columns):  # Creates an array of frequency where each index corresponds to the frequency at the same index of the data array
-        time_passed_sec = (1.0 / sample_rate) * i
-        frequency.append(chopper_sweep_start + (freq_per_sec * time_passed_sec))
-    frequency = np.array(frequency)
-
-    # Stack frequency with lock-in data
-    data = np.vstack((frequency, data_no_frequency)).transpose()
-
-    # Save data
-    np.save('data/col1_chopper_sweep_1kHz_to_120kHz_col2_R_col3_Theta', data)
-
-def sweep_chopper_freq_script():
-    wrapper.initialize()
-
-    # Constants
-    chopper_sweep_start = 1
-    chopper_sweep_stop = 140
-    chopper_sweep_step = 1
-    sample_rate = 512
-    time_constant = 1000.0
-    lock_in_time = 1.0
-
-    # Setup sweeper
-    wrapper.set_continuous_wave_freq(200)
-    wrapper.set_power(9.5)
-
-    # Setup chopper
-    wrapper.set_chopper_amplitude(1)
-    wrapper.set_chopper_on(True)
-
-    # Setup lock-in
-    wrapper.set_time_constant(time_constant)
-    wrapper.set_low_pass_slope(18)
-    wrapper.set_sensitivity(0.01)
-    wrapper.set_sample_rate(sample_rate)
-    wrapper.set_data('X', 'Y')
-
-    # Create a new array to save data to
-    data = np.array([0,0,0], float)  # This row will be deleted later
-
-    # Sweep the chopper frequency and record data
-    for freq in range(chopper_sweep_start, chopper_sweep_stop, chopper_sweep_step):
-        # Set the frequency to chop at
-        wrapper.set_chopper_frequency(freq)
-        # Sleep to allow lock-in to lock to new frequency and for time constant to average
-        time.sleep((time_constant*5.0)/1000.0)
-        # Start the scan
-        wrapper.start_scan()
-        # Sleep for one second to collect data
-        time.sleep((time_constant*5.0/1000.0) + lock_in_time)
-        # Stop the scan
-        wrapper.stop_scan()
-        # Get data from the lock-in amplifier and average
-        data_unaveraged_no_freq = wrapper.get_data()
-        data_averaged_no_freq = np.mean(data_unaveraged_no_freq, 1)
-        data_averaged = np.hstack(([freq], data_averaged_no_freq)).flatten()
-        data = np.vstack((data, data_averaged))
-
-    # Delete the first row in the collected data, as it was created to give the array shape earlier but holds no useful data
-    np.delete(data, 0, 0)
-
-    # Save data
-    np.save('data/col1_chopper_sweep_1kHz_to_120kHz_col2_R_col3_Theta', data)
-
-    # Close instruments
-    wrapper.close()
-
-
 def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=1000.0, sensitivity=0.01, slope=12, sample_rate=512, samples_to_collect=512, load_time=5, lock_in_time=1.0, chopper_amplitude=1, chopper_frequency=50, power=11.5, sweeper_frequency=250):
+    """
+    This method sweeps a parameter through a set of values. Any parameter can be chosen. If the chosen parameter is represented in one of this functions arguments, whatever is entered for that argument will be ignored,
+    :param parameter_set_func: The function that sets the parameter the user wishes to sweep through, i.e. wrapper.set_continuous_wave_freq.
+    :param values_to_sweep: The values to sweep the parameter through, i.e. range(200, 301, 2),
+    :param time_constant: The lock-in amplifier time constant.
+    :param sensitivity: The lock-in amplifier sensitivity.
+    :param slope: The lock-in amplifier roll off slope in dB/octave.
+    :param sample_rate: The lock-in amplifier sample rate.
+    :param samples_to_collect: The number of samples to collect.
+    :param load_time: The amount of time to give the instruments to finish setting up before data collection begins.
+    :param lock_in_time: The amount of time to give the lock in amplifier to lock back onto the reference signal after a parameter is changed.
+    :param chopper_amplitude: The amplitude of the chopper signal.
+    :param chopper_frequency: The frequency of the chopper signal.
+    :param power: The power of the sweeper.
+    :param sweeper_frequency: The frequency of the sweeper.
+    :return: The data collected, where the first column is frequency, the second column is X, and the third column is Y.
+    """
     wrapper.initialize()
 
     # Setup sweeper
@@ -174,7 +77,7 @@ def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=1000.0, s
     return data
 
 
-def generate_bode_plot(data, sensitivity=0):
+def generate_bode_plot(data):
     """
     Generates the appropriate plots from the given data.
     :param data: A n by 3 array, where there are n data points, column 0 is frequency, column 1 is x, and column 2 is y
@@ -186,6 +89,13 @@ def generate_bode_plot(data, sensitivity=0):
     y = data[:,2]
     response = np.sqrt(np.add(np.square(x), np.square(y))) # r=sqrt(x^2+y^2)
     phase = np.arctan2(y,x) # theta=arctan(y/x)]
+    phase_deg = np.degrees(phase)
+
+    # Determine axis scales
+    x_max = np.amax(x)
+    y_max = np.amax(y)
+    subplot1_max = max(x_max, y_max)
+
 
     # Define and stylize plots
 
@@ -194,27 +104,24 @@ def generate_bode_plot(data, sensitivity=0):
     plt.subplot(3,1,1)
     plt.plot(freq, x, 'b-')
     plt.plot(freq, y, 'g-')
-    plt.ylabel('(uV)')
+    plt.ylabel('A [V]')
 
     plt.subplot(3,1,2)
     plt.plot(freq, response, 'k-')
-    plt.ylabel('Response (uV)')
+    plt.ylabel('Response [V]')
 
     plt.subplot(3,1,3)
-    plt.plot(freq, phase, 'k-')
-    plt.ylabel('Phase (radians)')
-    plt.xlabel('Frequency (kHz)')
+    plt.plot(freq, phase_deg, 'k-')
+    plt.ylabel('Phase [degrees]')
+    plt.xlabel('Frequency [GHz]')
 
     plt.show()
 
 
 if __name__ == '__main__':
-    #sweep_chopper_freq_script()
 
-    #data = np.load('data/col1_chopper_sweep_1kHz_to_120kHz_col2_R_col3_Theta.npy')
-    #data = np.delete(data, 0, 0)
-    #generate_bode_plot(data)
+    #data = sweep_parameter(wrapper.set_continuous_wave_freq, range(200, 301, 2), time_constant=300, sensitivity=2, slope=12, sample_rate=512, samples_to_collect=512, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=50, power=11.5)
+    #np.save('data/col1_sweeper_sweep_200GHz_to_300GHz_col2_X_col3_Y', data)
 
-    data = sweep_parameter(wrapper.set_continuous_wave_freq, range(200, 301, 2), time_constant=300, sensitivity=2, slope=12, sample_rate=512, samples_to_collect=512, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=50, power=11.5)
-    np.save('data/col1_sweeper_sweep_200GHz_to_300GHz_col2_X_col3_Y', data)
+    data = np.load('data/col1_sweeper_sweep_200GHz_to_300GHz_col2_X_col3_Y.npy')
     generate_bode_plot(data)
