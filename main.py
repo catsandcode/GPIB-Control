8123fb3381eb4e3edfc9b78a1c3a634d3af879dc
catsandcode <jc.roth73@gmail.com>
2017-06-23 17:40:49 -0700

    Broke code down into a more modular form

--- main.py
+++ main.py
@@ -66,9 +66,10 @@ def sweep_chopper_freq_script():
     # Constants
     chopper_sweep_start = 1
     chopper_sweep_stop = 140
-    chopper_sweep_step = 10
+    chopper_sweep_step = 1
     sample_rate = 512
-    lock_in_sensitivity = 300.0
+    time_constant = 1000.0
+    lock_in_time = 1.0
 
     # Setup sweeper
     wrapper.set_continuous_wave_freq(200)
@@ -79,25 +80,25 @@ def sweep_chopper_freq_script():
     wrapper.set_chopper_on(True)
 
     # Setup lock-in
-    wrapper.set_time_constant(lock_in_sensitivity)
+    wrapper.set_time_constant(time_constant)
     wrapper.set_low_pass_slope(18)
     wrapper.set_sensitivity(0.01)
     wrapper.set_sample_rate(sample_rate)
     wrapper.set_data('X', 'Y')
 
     # Create a new array to save data to
-    data = np.array([0,0,0], float)
+    data = np.array([0,0,0], float)  # This row will be deleted later
 
     # Sweep the chopper frequency and record data
     for freq in range(chopper_sweep_start, chopper_sweep_stop, chopper_sweep_step):
         # Set the frequency to chop at
         wrapper.set_chopper_frequency(freq)
-        # Sleep to allow lock-in to lock to new frequency
-        time.sleep(1)
+        # Sleep to allow lock-in to lock to new frequency and for time constant to average
+        time.sleep((time_constant*5.0)/1000.0)
         # Start the scan
         wrapper.start_scan()
         # Sleep for one second to collect data
-        time.sleep((lock_in_sensitivity*5.0)/1000.0)
+        time.sleep((time_constant*5.0/1000.0) + lock_in_time)
         # Stop the scan
         wrapper.stop_scan()
         # Get data from the lock-in amplifier and average
@@ -106,37 +107,113 @@ def sweep_chopper_freq_script():
         data_averaged = np.hstack(([freq], data_averaged_no_freq)).flatten()
         data = np.vstack((data, data_averaged))
 
-    print data
+    # Delete the first row in the collected data, as it was created to give the array shape earlier but holds no useful data
+    np.delete(data, 0, 0)
 
+    # Save data
+    np.save('data/col1_chopper_sweep_1kHz_to_120kHz_col2_R_col3_Theta', data)
+
+    # Close instruments
+    wrapper.close()
+
+
+def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=1000.0, sensitivity=0.01, slope=12, sample_rate=512, samples_to_collect=512, load_time=5, lock_in_time=1.0, chopper_amplitude=1, chopper_frequency=50, power=11.5, sweeper_frequency=250):
+    wrapper.initialize()
+
+    # Setup sweeper
+    wrapper.set_continuous_wave_freq(sweeper_frequency)
+    wrapper.set_power(power)
+
+    # Setup chopper
+    wrapper.set_chopper_amplitude(chopper_amplitude)
+    wrapper.set_chopper_frequency(chopper_frequency)
+    wrapper.set_chopper_on(True)
+
+    # Setup lock-in
+    wrapper.set_time_constant(time_constant)
+    wrapper.set_sensitivity(sensitivity)
+    wrapper.set_low_pass_slope(slope)
+    wrapper.set_sample_rate(sample_rate)
+    wrapper.set_data('X', 'Y')
+
+    # Sleep to allow instruments to adjust settings
+    time.sleep(load_time)
+
+    # Do some math
+    data_collection_sleep_time = float(samples_to_collect)/float(sample_rate)
+
+    # Create a new array to save data to
+    data = np.array([0,0,0], float)  # This row will be deleted later
+
+    # Sweep the selected parameter and record data
+    for value in values_to_sweep:
+        print 'At sweep value ' + str(value)
+        # Set selected parameter to the given value
+        parameter_set_func(value)
+        # Sleep to allow lock-in to lock to new frequency and for time constant to average
+        time.sleep((time_constant * 5.0 / 1000.0) + lock_in_time)  # Sleep for five time constants plus the lock_in_time
+        # Start the scan
+        wrapper.start_scan()
+        # Sleep for one second to collect data
+        time.sleep(data_collection_sleep_time)
+        # Stop the scan
+        wrapper.stop_scan()
+        # Get data from the lock-in amplifier and average
+        data_unaveraged_no_freq = wrapper.get_data()
+        data_averaged_no_freq = np.mean(data_unaveraged_no_freq, 1)
+        data_averaged = np.hstack(([value], data_averaged_no_freq)).flatten()
+        data = np.vstack((data, data_averaged))
+
+    # Delete the first row in the collected data, as it was created to give the array shape earlier but holds no useful data
+    np.delete(data, 0, 0)
+
+    # Close instruments
+    wrapper.close()
+
+    # Return data
+    return data
+
+
+def generate_bode_plot(data, sensitivity=0):
+    """
+    Generates the appropriate plots from the given data.
+    :param data: A n by 3 array, where there are n data points, column 0 is frequency, column 1 is x, and column 2 is y
+    """
+
+    # Generate response and phase from the data array
     freq = data[:,0]
     x = data[:,1]
     y = data[:,2]
     response = np.sqrt(np.add(np.square(x), np.square(y))) # r=sqrt(x^2+y^2)
-    theta = np.arctan(np.divide(y,x)) # theta=arctan(y/x)
+    phase = np.arctan2(y,x) # theta=arctan(y/x)]
 
+    # Define and stylize plots
 
     plt.figure(1)
 
-    plt.subplot(2,2,1)
-    plt.plot(freq, response, 'g-')
+    plt.subplot(3,1,1)
+    plt.plot(freq, x, 'b-')
+    plt.plot(freq, y, 'g-')
+    plt.ylabel('(uV)')
 
-    plt.subplot(2,2,2)
-    plt.plot(freq, phase, 'r-')
+    plt.subplot(3,1,2)
+    plt.plot(freq, response, 'k-')
+    plt.ylabel('Response (uV)')
 
-    plt.subplot(2,2,3)
-    plt.plot(freq, phase, 'r-')
-
-    plt.subplot(2,2,4)
-    plt.plot(freq, phase, 'r-')
-
+    plt.subplot(3,1,3)
+    plt.plot(freq, phase, 'k-')
+    plt.ylabel('Phase (radians)')
+    plt.xlabel('Frequency (kHz)')
 
     plt.show()
 
-    # Stack frequency with lock-in data
-    #data = np.vstack((frequency, data_no_frequency)).transpose()
-
-    # Save data
-    #np.save('data/col1_chopper_sweep_1kHz_to_120kHz_col2_R_col3_Theta', data)
 
 if __name__ == '__main__':
-    sweep_chopper_freq_script()
+    #sweep_chopper_freq_script()
+
+    #data = np.load('data/col1_chopper_sweep_1kHz_to_120kHz_col2_R_col3_Theta.npy')
+    #data = np.delete(data, 0, 0)
+    #generate_bode_plot(data)
+
+    data = sweep_parameter(wrapper.set_continuous_wave_freq, [10.0*18, 11.0*18, 12.0*18, 12.5*18, 13.0*18, 13.5*18, 14.0*18, 14.5*18, 15.0*18, 15.5*18, 16.0*18, 17.0*18, 18.0*18, 19.0*18, 20.0*18], time_constant=300, sensitivity=2, slope=12, sample_rate=512, samples_to_collect=512, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=50, power=11.5)
+    print data
