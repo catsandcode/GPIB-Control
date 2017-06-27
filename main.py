b1d516c87aab827a916a1000bbcb2bf905b792db
catsandcode <jc.roth73@gmail.com>
2017-06-27 09:48:12 -0700

    Organized a little and wrote new test

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=1000.0, s
 
     # Do some math
     data_collection_sleep_time = float(samples_to_collect)/float(sample_rate)
-
     # Create a new array to save data to
     data = np.array([0,0,0], float)  # This row will be deleted later
 
@@ -106,12 +105,12 @@ def generate_bode_plot(*data_list):
         plt.subplot(3,1,1)
         plt.plot(freq, x, 'b-')
         plt.plot(freq, y, 'g-')
-        plt.yscale('log')
+        #plt.yscale('log')
         plt.ylabel('A [V]')
 
         plt.subplot(3,1,2)
         plt.plot(freq, response, 'k-')
-        plt.yscale('log')
+        #plt.yscale('log')
         plt.ylabel('Response [V]')
 
         plt.subplot(3,1,3)
@@ -124,6 +123,13 @@ def generate_bode_plot(*data_list):
 
 
 def generate_frequency_list(start, end, step):
+    """
+    Generates the list of frequencies between start and end with a step size of step.
+    :param start: The start frequency
+    :param end: The end frequency
+    :param step: The step in between frequencies
+    :return: The list of frequencies
+    """
     to_return = []
     current_frequency = start
     while(current_frequency <= end):
@@ -131,25 +137,30 @@ def generate_frequency_list(start, end, step):
         current_frequency += step
     return to_return
 
-if __name__ == '__main__':
-
-    #data = sweep_parameter(wrapper.set_continuous_wave_freq, generate_frequency_list(230,250,0.2), time_constant=3000, sensitivity=0.005, slope=12, sample_rate=512, samples_to_collect=512, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=50, power=10)
-    #np.save('data/col1_sweeper_sweep_200GHz_to_300GHz_col2_X_col3_Y_take2', data)
-
-    filt = np.load('data/filter.npy')
-    no_filter = np.load('data/no_filter.npy')
-
-    freq = filt[:, 0]
-    filter_x = filt[:, 1]
-    filter_y = filt[:, 2]
-    no_filter_x = no_filter[:, 1]
-    no_filter_y = no_filter[:, 2]
-
-    subtracted_x = np.subtract(filter_x, no_filter_x)
-    subtracted_y = np.subtract(filter_y, no_filter_y)
-
+def subtract_reference(reference, sample):
+    # Extract x and y from the reference and sample
+    freq = sample[:, 0]
+    sample_x = sample[:, 1]
+    sample_y = sample[:, 2]
+    reference_x = reference[:, 1]
+    reference_y = reference[:, 2]
+    # Divide sample x by reference x, do same for y
+    subtracted_x = np.divide(sample_x, reference_x)
+    subtracted_y = np.divide(sample_y, reference_y)
+    # Construct a new array of the subtracted data
     subtracted = np.vstack((freq, subtracted_x, subtracted_y)).transpose()
-    generate_bode_plot(filt, no_filter, subtracted)
+    # Return the new array
+    return subtracted
 
-    #data = sweep_parameter(wrapper.set_continuous_wave_freq, generate_frequency_list(200,300,1), time_constant=1000, sensitivity=0.5, slope=12, sample_rate=512, samples_to_collect=512, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=50, power=10)
-    #np.save('data/filter', data)
+
+if __name__ == '__main__':
+    for i in range(10):
+        data = sweep_parameter(wrapper.set_continuous_wave_freq, generate_frequency_list(220, 260, 1), time_constant=1000, sensitivity=0.5, slope=12, sample_rate=8, samples_to_collect=8, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=20, power=10)
+        np.save('data/repeat_sweep_trial_' + str(i), data)
+
+    data_list = []
+    for i in range(10):
+        data = np.load('data/repeat_sweep_trial_' + str(i))
+        data_list.append(data)
+
+    generate_bode_plot(data_list)
