07e0d7aa2ed8aed80e4684b81bef9bbca64b44aa
catsandcode <jc.roth73@gmail.com>
2017-06-28 09:19:29 -0700

    Removed old test code and updated sweep_parameter function (see description for more)
    The sweep_parameter function was updated to snap values instead of sample over a period of time. This is faster. Sampling variables over a period of time didn't seem to offer any accuracy advantages (as the time constant of the lock-in should do averaging by itself). Variable names were updated and the wrapper calls were updated to reflect the fact that the pasternack rf source is now being used instead of the HP8350B.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import time, wrapper
-from instruments import PasternackPE11S390
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=1000.0, sensitivity=0.01, slope=12, sample_rate=512, samples_to_collect=512, load_time=5, lock_in_time=1.0, chopper_amplitude=1, chopper_frequency=50, power=11.5, sweeper_frequency=250):
+def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=300, sensitivity=500, slope=12, load_time=5, lock_in_time=1.0, chopper_amplitude=5, chopper_frequency=5, power=15, freq_synth_frequency=250):
     """
     This method sweeps a parameter through a set of values. Any parameter can be chosen. If the chosen parameter is represented in one of this functions arguments, whatever is entered for that argument will be ignored,
     :param parameter_set_func: The function that sets the parameter the user wishes to sweep through, i.e. wrapper.set_continuous_wave_freq.
@@ -24,9 +23,9 @@ def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=1000.0, s
     """
     wrapper.initialize()
 
-    # Setup sweeper
-    wrapper.set_continuous_wave_freq(sweeper_frequency)
-    wrapper.set_power(power)
+    # Setup the frequency synthesizer
+    wrapper.set_freq_synth_frequency(freq_synth_frequency)
+    wrapper.set_freq_synth_power(power)
 
     # Setup chopper
     wrapper.set_chopper_amplitude(chopper_amplitude)
@@ -37,34 +36,26 @@ def sweep_parameter(parameter_set_func, values_to_sweep, time_constant=1000.0, s
     wrapper.set_time_constant(time_constant)
     wrapper.set_sensitivity(sensitivity)
     wrapper.set_low_pass_slope(slope)
-    wrapper.set_sample_rate(sample_rate)
-    wrapper.set_data('X', 'Y')
 
     # Sleep to allow instruments to adjust settings
     time.sleep(load_time)
 
-    # Do some math
-    data_collection_sleep_time = float(samples_to_collect)/float(sample_rate)
     # Create a new array to save data to
     data = np.array([0,0,0], float)  # This row will be deleted later
 
     # Sweep the selected parameter and record data
     for value in values_to_sweep:
         print 'At sweep value ' + str(value)
+
         # Set selected parameter to the given value
         parameter_set_func(value)
+
         # Sleep to allow lock-in to lock to new frequency and for time constant to average
         time.sleep((time_constant * 5.0 / 1000.0) + lock_in_time)  # Sleep for five time constants plus the lock_in_time
-        # Start the scan
-        wrapper.start_scan()
-        # Sleep for one second to collect data
-        time.sleep(data_collection_sleep_time)
-        # Stop the scan
-        wrapper.stop_scan()
-        # Get data from the lock-in amplifier and average
-        data_unaveraged_no_freq = wrapper.get_data()
-        data_averaged_no_freq = np.mean(data_unaveraged_no_freq, 1)
-        data_averaged = np.hstack(([value], data_averaged_no_freq)).flatten()
+
+        # Get data from the lock-in amplifier and and add it to the data array
+        (x, y) = wrapper.snap_data()
+        data_averaged = np.hstack((value, x, y))
         data = np.vstack((data, data_averaged))
 
     # Delete the first row in the collected data, as it was created to give the array shape earlier but holds no useful data
@@ -192,7 +183,7 @@ def subtract2(num, ref, sample):
     plt.ylabel('Response')
 
 
-def sweep_parameter2(parameter_set_func, values_to_sweep, time_constant=1000.0, sensitivity=0.01, slope=12, sample_rate=512, samples_to_collect=512, load_time=5, lock_in_time=1.0, chopper_amplitude=1, chopper_frequency=50, power=7.5, sweeper_frequency=250):
+def sweep_parameter2(parameter_set_func, values_to_sweep, time_constant=1000.0, sensitivity=0.01, slope=12, sample_rate=512, samples_to_collect=512, load_time=5, lock_in_time=1.0, chopper_amplitude=1, chopper_frequency=50, power=7.5, freq_synth_frequency=250):
     """
     This method sweeps a parameter through a set of values. Any parameter can be chosen. If the chosen parameter is represented in one of this functions arguments, whatever is entered for that argument will be ignored,
     :param parameter_set_func: The function that sets the parameter the user wishes to sweep through, i.e. wrapper.set_continuous_wave_freq.
@@ -276,94 +267,3 @@ def sweep_parameter2(parameter_set_func, values_to_sweep, time_constant=1000.0, 
 
 if __name__ == '__main__':
 
-    power = 15
-    chopper_amplitude = 5
-    chopper_freq = 5
-    time_constant = 300
-    sensitivity = 500
-    slope = 12
-    load_time = 2
-    lock_in_time = 0.5
-
-    wrapper.initialize()
-
-    freq_synth = PasternackPE11S390(wrapper.resource_manager, 'USB0::0x2012::0x0011::5001::INSTR')
-    freq_synth.open()
-    freq_synth.initialize_instrument()
-
-    # Setup sweeper
-    freq_synth.set_power(power)
-
-    # Setup chopper
-    wrapper.set_chopper_amplitude(chopper_amplitude)
-    wrapper.set_chopper_frequency(chopper_freq)
-    wrapper.set_chopper_on(True)
-
-    # Setup lock-in
-    wrapper.set_time_constant(time_constant)
-    wrapper.set_sensitivity(sensitivity)
-    wrapper.set_low_pass_slope(slope)
-    wrapper.set_data('X', 'Y')
-
-    # Sleep to allow instruments to adjust settings
-    time.sleep(load_time)
-
-    # Create a new array to save data to
-    data = np.array([0, 0, 0], float)  # This row will be deleted later
-
-    for freq in generate_frequency_list(12, 16, 0.2):
-        # Set selected parameter to the given value
-        freq_synth.set_frequency(freq)
-        print 'At frequency value ' + str(freq_synth.get_frequency())
-        # Sleep to allow lock-in to lock to new frequency and for time constant to average
-        time.sleep((time_constant * 5.0 / 1000.0) + lock_in_time)  # Sleep for five time constants plus the lock_in_time
-        # Snap the data
-        (x, y) = wrapper.snap_data()
-        print 'x:' + str(x)
-        print 'y:' + str(y)
-        # Get data from the lock-in amplifier and average
-        data_averaged = np.hstack((freq, x, y))
-        data = np.vstack((data, data_averaged))
-
-    # Delete the first row in the collected data, as it was created to give the array shape earlier but holds no useful data
-    data = np.delete(data, 0, 0)
-
-    freq_synth.close()
-    # Close instruments
-    wrapper.close()
-
-    freq = data[:, 0]
-    x = data[:, 1]
-    y = data[:, 2]
-
-    plt.figure(0)
-
-    plt.plot(freq, x, 'r-')
-    plt.plot(freq, y, 'b-')
-
-    plt.show()
-
-    """
-    data_list = []
-    for i in range(10):
-        data = np.load('data/repeat_sweep_trial_' + str(i)+'.npy')
-        data_list.append(data)
-
-    #generate_bode_plot(data_list)
-    
-    subtracted_list = []
-    for i in range(1, 10):
-        subtracted_list.append(subtract_reference(data_list[0], data_list[i]))
-    plt.show()
-    
-
-    #sweep_parameter2(wrapper.set_sample_rate, [512, 256, 128, 8, 1], time_constant=1000, sensitivity=0.5, slope=12, sample_rate=8, samples_to_collect=8, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=20, power=7.5)
-
-    for i in range(10):
-        data = sweep_parameter(wrapper.set_continuous_wave_freq, generate_frequency_list(220, 260, 1), time_constant=1000, sensitivity=0.5, slope=12, sample_rate=8, samples_to_collect=8, lock_in_time=1.0, chopper_amplitude=1.0, chopper_frequency=5, power=7.5)
-        np.save('data/repeat_sweep_trial_' + str(i), data)
-
-    data = np.load('data/data collection week 1/col1_sweeper_sweep_200GHz_to_300GHz_col2_X_col3_Y.npy')
-    print data
-    generate_bode_plot([data])
-    """
