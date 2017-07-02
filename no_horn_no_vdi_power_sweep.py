de9d17cb7638e945462c11bc7df13189b858e7ca
catsandcode <jc.roth73@gmail.com>
2017-07-01 23:02:03 -0700

    Fixed issue in experiment
    multiplier was set to 18, but should have been set to 1

--- no_horn_no_vdi_power_sweep.py
+++ no_horn_no_vdi_power_sweep.py
@@ -2,4 +2,4 @@ from control import daq, experiment_wrapper
 import numpy as np
 
 for power in np.arange(8.0, 15.0, 0.5):
-    daq.sweep_parameter(experiment_wrapper.set_freq_synth_frequency, np.arange(12.5, 16.5, 0.1), power=power, time_constant=100, sensitivity=0.005, load_time=3, lock_in_time=0.1, save_path='no_horn_no_vdi_power_sweep_data_folder/power_' + str(power))
+    daq.sweep_parameter(experiment_wrapper.set_freq_synth_frequency, np.arange(12.5, 16.5, 0.1), power=power, time_constant=100, sensitivity=0.005, load_time=3, lock_in_time=0.1, multiplier=1, save_path='no_horn_no_vdi_power_sweep_data_folder/power_' + str(power))
