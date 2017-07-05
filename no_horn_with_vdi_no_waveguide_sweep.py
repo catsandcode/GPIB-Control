f677f539cf5c7b8e4f7391da55842c1a9c6dde15
catsandcode <jc.roth73@gmail.com>
2017-07-05 16:44:18 -0700

    Renamed file

--- no_horn_with_vdi_no_waveguide_sweep.py
+++ no_horn_with_vdi_no_waveguide_sweep.py
@@ -4,4 +4,4 @@ import numpy as np
 
 for i in range(0, 3):
     sweep_parameter(set_freq_synth_frequency, np.linspace(12.5, 16.5, num=500, endpoint=True), time_constant=100,  sensitivity=500, load_time=0.5, lock_in_time=0.1, multiplier=1,
-                    save_path='no_horn_no_vdi_with_waveguide_sweep_data_folder/sweep_num_' + str(i))
+                    save_path='no_horn_with_vdi_no_waveguide_sweep_data_folder/sweep_num_' + str(i))
