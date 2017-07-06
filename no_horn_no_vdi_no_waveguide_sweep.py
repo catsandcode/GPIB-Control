048be209bc90fc2fd3f308c510466d3ac511e6a2
catsandcode <jc.roth73@gmail.com>
2017-07-06 09:52:47 -0700

    Further experiment updates

--- no_horn_no_vdi_no_waveguide_sweep.py
+++ no_horn_no_vdi_no_waveguide_sweep.py
@@ -3,5 +3,5 @@ from control.experiment_wrapper import set_freq_synth_frequency
 import numpy as np
 
 for i in range(0, 3):
-    sweep_parameter(set_freq_synth_frequency, np.linspace(12.5, 16.5, num=500, endpoint=True), time_constant=300,  sensitivity=500, load_time=0.5, lock_in_time=0.1, multiplier=1,
+    sweep_parameter(set_freq_synth_frequency, np.linspace(12.5, 16.5, num=500, endpoint=True), time_constant=100,  sensitivity=500, load_time=0.5, lock_in_time=0.1, multiplier=1,
                     save_path='no_horn_no_vdi_no_waveguide_sweep_data_folder/sweep_num_' + str(i))
