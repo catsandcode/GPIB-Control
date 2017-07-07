7ab9e491bd6a6ecf7915f185c1c0c7f1e8f23a24
catsandcode <jc.roth73@gmail.com>
2017-07-06 18:53:38 -0700

    New experiment

---
+++ no_horn_no_vdi_with_waveguide_sweep.py
@@ -0,0 +1,7 @@
+from control.daq import sweep_parameter
+from control.experiment_wrapper import set_freq_synth_frequency
+import numpy as np
+
+for i in range(0, 3):
+    sweep_parameter(set_freq_synth_frequency, np.linspace(12.5, 16.5, num=150, endpoint=True), time_constant=100,  sensitivity=500, load_time=4, lock_in_time=3, multiplier=1,
+                    save_path='no_horn_no_vdi_with_waveguide_sweep_data_folder/sweep_num_' + str(i))
