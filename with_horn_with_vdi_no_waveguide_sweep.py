feef27bf07d540fc477ee740f0074103d804e5a6
catsandcode <jc.roth73@gmail.com>
2017-07-07 17:11:04 -0700

    Updated lock_in_time in with_horn_with_vdi_no_waveguide_sweep.py

--- with_horn_with_vdi_no_waveguide_sweep.py
+++ with_horn_with_vdi_no_waveguide_sweep.py
@@ -7,5 +7,5 @@ Uses a time constant of 100ms, 12dB/oct, 0.2mV sensitivity, load time of 4s, loc
 """
 
 for i in range(0, 3):
-    sweep_parameter(set_freq_synth_frequency, np.linspace(12.5, 16.5, num=160, endpoint=True), time_constant=300,  sensitivity=0.2, load_time=4, lock_in_time=2, multiplier=1,
+    sweep_parameter(set_freq_synth_frequency, np.linspace(12.5, 16.5, num=160, endpoint=True), time_constant=300,  sensitivity=0.2, load_time=4, lock_in_time=3, multiplier=1,
                     save_path='with_horn_with_vdi_no_waveguide_sweep_data_folder/sweep_num_' + str(i))
