b1f7af720db19d62f5fa1ffd6c87c3bff174aff1
catsandcode <jc.roth73@gmail.com>
2017-07-11 15:53:28 -0700

    Uploaded zotefoam_window_test1.py

---
+++ zotefoam_window_test1.py
@@ -0,0 +1,28 @@
+from control.daq import sweep_parameter
+from control.experiment_wrapper import set_freq_synth_frequency
+import numpy as np
+import sys
+
+def wait_for_user_confirmation(instruction):
+    print instruction
+    while True:
+        s = raw_input('continue? y/[n]\n')
+        if s.lower() == 'y':
+            print 'continuing...'
+            break
+        elif s.lower() == 'n':
+            print 'exiting...'
+            sys.exit(0)
+
+wait_for_user_confirmation('please ensure that no filter is between the two antenna horns')
+
+sweep_parameter(set_freq_synth_frequency, np.linspace(225, 300, num=150, endpoint=True), time_constant=100,  sensitivity=0.2, load_time=4, lock_in_time=0, multiplier=18,
+                save_path='zotefoam_window_test1/no_filter')
+
+wait_for_user_confirmation('please place the filter between the two antenna horns')
+
+sweep_parameter(set_freq_synth_frequency, np.linspace(225, 300, num=150, endpoint=True), time_constant=100,  sensitivity=0.2, load_time=4, lock_in_time=0, multiplier=18,
+                save_path='zotefoam_window_test1/with_filter_sweep1')
+
+sweep_parameter(set_freq_synth_frequency, np.linspace(225, 300, num=150, endpoint=True), time_constant=100,  sensitivity=0.2, load_time=4, lock_in_time=0, multiplier=18,
+                save_path='zotefoam_window_test1/with_filter_sweep2')
