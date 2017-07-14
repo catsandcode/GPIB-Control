1f9f4192ab60a68057e940cd7685301097967c51
catsandcode <jc.roth73@gmail.com>
2017-07-14 16:07:16 -0700

    Fixed MAJOR error in sweep_tuning_test4.py

--- sweep_tuning_test4.py
+++ sweep_tuning_test4.py
@@ -8,7 +8,7 @@ Horns are 98.80mm apart.
 Uses a time constant of 100ms, 12dB/oct, 0.2mV sensitivity, load time of 4s, lock in time of 0s. Sweeps from 225GHz to 275GHz (horn output frequencies) in 250MHz steps. Returns values in volts.
 """
 for i in range(0, 3):
-    sweep_parameter(set_freq_synth_frequency, np.linspace(225.0, 275.0, num=200, endpoint=True), time_constant=100,  sensitivity=0.2, load_time=4, lock_in_time=0, multiplier=1,
+    sweep_parameter(set_freq_synth_frequency, np.linspace(225.0, 275.0, num=200, endpoint=True), time_constant=100,  sensitivity=0.2, load_time=4, lock_in_time=0, multiplier=18,
                     save_path='sweep_tuning_test4/high_time_const_sweep' + str(i))
 
 """
@@ -17,5 +17,5 @@ Horns are 98.80mm apart.
 Uses a time constant of 30ms, 12dB/oct, 0.2mV sensitivity, load time of 4s, lock in time of 0s. Sweeps from 225GHz to 275GHz (horn output frequencies) in 250MHz steps. Returns values in volts.
 """
 for i in range(0, 3):
-    sweep_parameter(set_freq_synth_frequency, np.linspace(225.0, 275.0, num=200, endpoint=True), time_constant=30,  sensitivity=0.2, load_time=4, lock_in_time=0, multiplier=1,
+    sweep_parameter(set_freq_synth_frequency, np.linspace(225.0, 275.0, num=200, endpoint=True), time_constant=30,  sensitivity=0.2, load_time=4, lock_in_time=0, multiplier=18,
                     save_path='sweep_tuning_test4/low_time_const_sweep' + str(i))
