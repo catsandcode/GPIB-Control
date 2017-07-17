2322b40c8eca9c9ff49923a130917d89e3129d4d
catsandcode <jc.roth73@gmail.com>
2017-07-17 14:02:03 -0700

    Uploaded higher resolution experiment

---
+++ alumina_filter_test5.py
@@ -0,0 +1,54 @@
+from control.daq import sweep_parameter
+from control.experiment_wrapper import set_freq_synth_frequency
+import numpy as np
+import sys
+
+"""
+Horns are 98.57mm apart.
+
+Uses a time constant of 100ms, 12dB/oct, 0.2mV sensitivity, load time of 4s, lock in time of 0s. Sweeps from 225GHz to 275GHz in 100MHz steps. Returns values in volts.
+
+Takes four sweeps, one without filter, then two with filter and finally one without.
+"""
+
+
+def wait_for_user_confirmation(instruction):
+    print instruction
+    while True:
+        s = raw_input('continue? y/[n]\n')
+        if s.lower() == 'y':
+            print 'continuing...'
+            break
+        elif s.lower() == 'n':
+            print 'exiting...'
+            sys.exit(0)
+
+# Settings
+freq_start = 225
+freq_end = 275
+num_steps = 500
+time_const = 100
+chop_freq = 1
+sens = 0.2
+load_time = 4
+lock_time = 0
+multiplier = 18
+
+# Start tests
+wait_for_user_confirmation('please ensure that no filter is between the two antenna horns')
+
+sweep_parameter(set_freq_synth_frequency, np.linspace(freq_start, freq_end, num=num_steps, endpoint=True), time_constant=time_const, chopper_frequency=chop_freq, sensitivity=sens, load_time=load_time, lock_in_time=lock_time, multiplier=multiplier,
+                    save_path='alumina_filter_test3/no_filter0')
+
+wait_for_user_confirmation('please place the filter between the two antenna horns')
+
+sweep_parameter(set_freq_synth_frequency, np.linspace(freq_start, freq_end, num=num_steps, endpoint=True), time_constant=time_const, chopper_frequency=chop_freq, sensitivity=sens, load_time=load_time, lock_in_time=lock_time, multiplier=multiplier,
+                    save_path='alumina_filter_test3/with_filter0')
+
+sweep_parameter(set_freq_synth_frequency, np.linspace(freq_start, freq_end, num=num_steps, endpoint=True), time_constant=time_const, chopper_frequency=chop_freq, sensitivity=sens, load_time=load_time, lock_in_time=lock_time, multiplier=multiplier,
+                    save_path='alumina_filter_test3/with_filter1')
+
+wait_for_user_confirmation('please remove the filter between the two antenna horns')
+
+sweep_parameter(set_freq_synth_frequency, np.linspace(freq_start, freq_end, num=num_steps, endpoint=True), time_constant=time_const, chopper_frequency=chop_freq, sensitivity=sens, load_time=load_time, lock_in_time=lock_time, multiplier=multiplier,
+                    save_path='alumina_filter_test5/no_filter1')
